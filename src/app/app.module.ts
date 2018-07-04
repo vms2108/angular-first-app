eab5becebc6812ad21809b0dbb45c2988aa49f61
mikhail <vms2108@mail.ru>
2018-07-04 16:55:55 +0300

    reactive forms и валидация

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { AppProductsComponent } from './products/products.component';
@@ -16,7 +16,8 @@ import { AppSortingComponent } from './sorting/sorting.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/products/products-add/products-add.component.ts
+++ src/app/products/products-add/products-add.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnInit} from '@angular/core';
+import { FormGroup, FormControl, Validators} from '@angular/forms';
 
 @Component ({
   selector: 'app-products-add',
@@ -6,12 +7,50 @@ import { Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
   styleUrls: ['products-add.component.scss']
 })
 
-export class AppProductsAddComponent {
+export class AppProductsAddComponent implements OnInit {
   @Input() CategoriesFrom: any = null;
   @Input() id = 1;
   @Output() NewProduct = new EventEmitter <any>();
+  category: string;
+  name: string;
+  price: number;
   addProduct(id: number, name: string, category: string, price: number) {
     this.NewProduct.emit({id, name, category, price});
+    this.myForm = new FormGroup({
+
+      'userName': new FormControl('', [
+                          Validators.required,
+                          Validators.pattern('[a-zA-Z\s]{2,10}')
+                        ]),
+      'userCategory': new FormControl('', [
+                          Validators.required
+                      ]),
+      'userPrice': new FormControl('', [
+                          Validators.required,
+                          Validators.pattern('[0-9]{2,9}')
+                        ])
+    });
   }
+
+  ngOnInit() {}
+
+  // tslint:disable-next-line:member-ordering
+  myForm: FormGroup;
+    constructor() {
+        this.myForm = new FormGroup({
+
+            'userName': new FormControl('', [
+                                Validators.required,
+                                Validators.pattern('[a-zA-Z\s]{2,10}')
+                              ]),
+            'userCategory': new FormControl('', [
+                                Validators.required
+                            ]),
+            'userPrice': new FormControl('', [
+                                Validators.required,
+                                Validators.pattern('[0-9]{2,9}')
+                              ])
+        });
+    }
 
 }
